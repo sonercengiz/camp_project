af90b7f560c6e81645084587ccc3e50484becf92
Soner Cengiz <soner.cengiz@tregitim.com.tr>
2022-05-12 01:26:40 +0300

    Add project files.

---
+++ camp_project/Controllers/CampgroundController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace camp_project.Controllers
+{
+    public class CampgroundController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
